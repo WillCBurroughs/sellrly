441b93d963445564c2fc0e25b18f70db639dd0ef
William <willcburroughs@gmail.com>
2025-07-31 19:18:12 -0400

    Adding secondary button start and new fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import PrimaryButton from './components/atoms/PrimaryButton';
+import SecondaryButton from './components/atoms/SecondaryButton';
+import './index.css';
 
 function App() {
   return (
     <>
       <PrimaryButton text = "Primary Button"/>
+      <h2 className='test'> Clash Grotesk </h2>
+      <SecondaryButton text = "Secondary Button"/> 
     </>
   );
 }
