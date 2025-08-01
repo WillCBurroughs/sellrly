8c417a171c103b0719e6018494ad39cc07c58e1a
William <willcburroughs@gmail.com>
2025-07-31 20:18:57 -0400

    Adding classes for sizing and new nav button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
 import PrimaryButton from './components/atoms/PrimaryButton';
 import SecondaryButton from './components/atoms/SecondaryButton';
-import './index.css';
+import NavBarItem from './components/atoms/NavBarItem';
 
 function App() {
   return (
     <>
+      <h1> H1 </h1>
+      <h2> H2 </h2>
+      <h3> H3 </h3>
+      <h4> H4 </h4>
+      <h5> H5 </h5>
+      <p className='blb'> Body | Large | Bold </p>
+      <p className='blr'> Body | Large | Regular </p>
+      <p className='bmb'> Body | Medium | Bold </p>
+      <p className='bmr'> Body | Medium | Regular </p>
+      <p className='bnb'> Body | Normal | Bold </p>
+      <p className='bnr'> Body | Normal | Regular </p>
+      <p className='stb'> Small Text Bold </p>
+      <p className='str'> Small Text Regular </p>
       <PrimaryButton text = "Primary Button"/>
-      <h2 className='test'> Clash Grotesk </h2>
       <SecondaryButton text = "Secondary Button"/> 
+      <NavBarItem text = "Who we serve"/> 
     </>
   );
 }
---
+++ src/components/atoms/NavBarItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../index.css'
+
+function NavBarItem(props) {
+  return (
+    <p className='NavItem'>
+      {props.text}
+    </p>
+  );
+}
+
+export default NavBarItem;
