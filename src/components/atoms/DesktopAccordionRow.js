2b9db3d755e93fc75edb284bc7f06a15965a8c55
William <willcburroughs@gmail.com>
2025-08-10 15:49:38 -0400

    Adding finished Faqs section

--- src/components/atoms/DesktopAccordionRow.js
+++ src/components/atoms/DesktopAccordionRow.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import DropDownActive from '../../images/DropDownActive.svg';
+import DropDownInactive from '../../images/DropDownArrowInactive.svg'
 
 const DesktopAccordionRow = ({ title, content }) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <div className="accordion-item">
-      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+      <div className={` ${isActive ? "accordion-title-Active" : "accordion-title"}`} onClick={() => setIsActive(!isActive)}>
         <div>{title}</div>
-        <div>{isActive ? '-' : '+'}</div>
+        <div>{isActive ? <img className = "DropDown" src={DropDownActive} alt='Drop Down Active'/> : <img className = "DropDown" src={DropDownInactive} alt='Drop Down Inactive'/>}</div>
       </div>
       {isActive && <div className="accordion-content">{content}</div>}
     </div>
