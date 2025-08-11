75bfa1c6c63cfc762337bfac7a7cdad072343383
William <willcburroughs@gmail.com>
2025-08-10 20:33:06 -0400

    Adding routing on Desktop navbar

--- src/components/molecules/NavBarDesktop.js
+++ src/components/molecules/NavBarDesktop.js
@@ -4,26 +4,27 @@ import LogoTagline from '../atoms/LogoTagline';
 import NavBarItem from '../atoms/NavBarItem';
 import PrimaryButton from '../atoms/PrimaryButton';
 import SecondaryButton from '../atoms/SecondaryButton';
+import { Link, NavLink } from "react-router-dom";
 
-function NavBarDesktop(props) {
+export default function NavBarDesktop() {
   return (
-    <div className='NavDesktop'>
+    <nav>
+      <div className='NavDesktop'>
         <div className='logoLeft'>
-            <LogoTagline/>
+          <LogoTagline />
         </div>
         <div className='linksNavDesktop'>
-            <NavBarItem text = "Home"/>
-            <NavBarItem text = "Services"/>
-            <NavBarItem text = "Who We Serve" />
-            <NavBarItem text = "About Us" />
-            <NavBarItem text = "Contact Us" />
+          <NavLink className="navlink-reset" to="/" end><NavBarItem text="Home" /></NavLink>
+          <NavLink className="navlink-reset" to="Services" end><NavBarItem text="Services" /></NavLink>
+          <NavLink className="navlink-reset" to="Who-We-Serve" end><NavBarItem text="Who We Serve" /></NavLink>
+          <NavLink className="navlink-reset" to="About-Us" end><NavBarItem text="About Us" /></NavLink>
+          <NavLink className="navlink-reset" to="Contact-Us" end><NavBarItem text="Contact Us" /></NavLink>
         </div>
         <div className='login-signup'>
-            <SecondaryButton text = "Log In"/>
-            <PrimaryButton text = "Sign Up"/>
+          <NavLink className="navlink-reset" to="Log-In" end><SecondaryButton text="Log In" /></NavLink>
+          <NavLink className="navlink-reset" to="Sign-Up" end><PrimaryButton text="Sign Up" /></NavLink>
         </div>
-    </div>
+      </div>
+    </nav>
   );
 }
-
-export default NavBarDesktop;
