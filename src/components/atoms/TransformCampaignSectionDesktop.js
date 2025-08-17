f77e08274600ec8c2d6b6e6a2e20fe7f69de9094
William <willcburroughs@gmail.com>
2025-08-16 21:41:52 -0400

    Finished transform campaign section

--- src/components/atoms/TransformCampaignSectionDesktop.js
+++ src/components/atoms/TransformCampaignSectionDesktop.js
@@ -2,12 +2,19 @@
 import React from 'react';
 import '../../index.css'
 import Shapes from '../../images/Shapes.svg'
+import SecondaryButton from './SecondaryButton';
 
 function TransformCampaignSectionDesktop(props) {
   return (
         <div className='TransformCampaignDesktop' id = {props.id}>
             <div className='TransformCampaignDesktopTextHolder'>
-
+                <h5 className='TransformSellingDesktop'>
+                    Transform your social selling experience
+                </h5>
+                <p className='TakePlatformCampaignDesktop'>
+                    Take your platform to the next level, amplify your reach, and sell more than ever before
+                </p>
+                <SecondaryButton text = "Start a Campaign Now" id = "StartCampaignNowBTN" />
             </div>
             <div className='TransformCampaignDesktopImageHolder'>
                 <img className='ImageInCampaignDesktop' src= {Shapes}/>
