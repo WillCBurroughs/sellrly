e1074bbf4987da11c7e0e916bc936e9c30dca78e
William <willcburroughs@gmail.com>
2025-08-16 21:33:13 -0400

    Adding Campaign Desktop image

--- src/components/atoms/TransformCampaignSectionDesktop.js
+++ src/components/atoms/TransformCampaignSectionDesktop.js
@@ -1,11 +1,17 @@
 
 import React from 'react';
 import '../../index.css'
+import Shapes from '../../images/Shapes.svg'
 
 function TransformCampaignSectionDesktop(props) {
   return (
         <div className='TransformCampaignDesktop' id = {props.id}>
-            
+            <div className='TransformCampaignDesktopTextHolder'>
+
+            </div>
+            <div className='TransformCampaignDesktopImageHolder'>
+                <img className='ImageInCampaignDesktop' src= {Shapes}/>
+            </div>
         </div>
   );
 }
