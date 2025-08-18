395c33a11092b5ba1c187a1fb0bbfeab60c9aa7d
William <willcburroughs@gmail.com>
2025-08-18 17:44:30 -0400

    Adding hold founders and gradients

---
+++ src/components/molecules/FoundersSectionDesktop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../index.css'
+
+function FoundersSectionDesktop(props) {
+  return (
+    <div className='holdFoundersDesktop'>
+        
+    </div>
+  );
+}
+
+export default FoundersSectionDesktop;
