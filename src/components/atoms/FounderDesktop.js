f7aaaace55935a73262c4c7a03adf6e1355698c1
William <willcburroughs@gmail.com>
2025-08-18 20:22:59 -0400

    Adding founder sections

--- src/components/atoms/FounderDesktop.js
+++ src/components/atoms/FounderDesktop.js
@@ -1,9 +1,27 @@
 import '../../index.css'
+import Founder from '../../images/Founder.png'
 
 function FounderDesktop(props) {
   return (
-    <div className='FounderDesktop'>
-
+    <div className='FounderDesktop' id={props.id}>
+        <div className='FounderTextDesktop'>
+            <span className='DecorationDesktop'></span>
+            <h3>
+                {props.founderName}
+            </h3>
+            <p className='blb' id = "FounderTitle">
+                {props.founderTitle}
+            </p>
+            <p>
+                {props.founderFirstLines}
+            </p>
+            <p>
+                {props.founderNextLines}
+            </p>
+        </div>
+        <div className='FounderImageDesktop'>
+            <img src= {Founder} alt='Founder'/>
+        </div>
     </div>
   );
 }
