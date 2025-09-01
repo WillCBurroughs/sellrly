8472c3a06f6dda091515d42dcf4744ddd7815040
William <willcburroughs@gmail.com>
2025-09-01 12:20:46 -0400

    adding non functional hamburger menu mobile

--- src/components/molecules/NavBarMobile.js
+++ src/components/molecules/NavBarMobile.js
@@ -1,29 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../index.css'
 import LogoTagline from '../atoms/LogoTagline';
 import NavBarItem from '../atoms/NavBarItem';
 import PrimaryButton from '../atoms/PrimaryButton';
 import SecondaryButton from '../atoms/SecondaryButton';
 import { Link, NavLink } from "react-router-dom";
+import hamburgerMenuClosed from '../../images/HamburgerMenu.png';
 
 export default function NavBarMobile() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav>
       <div className='NavDesktop'>
         <div className='logoLeft'>
           <LogoTagline />
         </div>
-        {/* <div className='linksNavDesktop'>
-          <NavLink className="navlink-reset" to="/" end><NavBarItem text="Home" /></NavLink>
-          <NavLink className="navlink-reset" to="Services" end><NavBarItem text="Services" /></NavLink>
-          <NavLink className="navlink-reset" to="Who-We-Serve" end><NavBarItem text="Who We Serve" /></NavLink>
-          <NavLink className="navlink-reset" to="About-Us" end><NavBarItem text="About Us" /></NavLink>
-          <NavLink className="navlink-reset" to="Contact-Us" end><NavBarItem text="Contact Us" /></NavLink>
+        <div className='hamburger'>
+          <img src={hamburgerMenuClosed} className='hamburgerMenu'/>
         </div>
-        <div className='login-signup'>
-          <NavLink className="navlink-reset" to="Log-In" end><SecondaryButton text="Log In" /></NavLink>
-          <NavLink className="navlink-reset" to="Sign-Up" end><PrimaryButton text="Sign Up" /></NavLink>
-        </div> */}
       </div>
     </nav>
   );
