1537c06eca23f4898f654684cff55af7dc4ebafc
William <willcburroughs@gmail.com>
2025-09-07 11:49:53 -0400

    adding video section

--- src/components/templates/HomePageMobile.js
+++ src/components/templates/HomePageMobile.js
@@ -1,12 +1,13 @@
 import React from "react";
 import '../../index.css';
 import HomeAboveFoldMobileMobile from "../molecules/HomeAboveFoldMobile";
-
+import VideoMobileHome from "../molecules/VideoMobileHome";
 
 function HomePageMobile(props) {
   return (
     <>
         <HomeAboveFoldMobileMobile/>
+        <VideoMobileHome/>
     </>
   );
 }
