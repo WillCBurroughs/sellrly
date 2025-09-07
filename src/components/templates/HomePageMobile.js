3b575ff2ecf805766cbe0d137e61cc30564ba32f
William <willcburroughs@gmail.com>
2025-09-07 11:01:25 -0400

    adding top section above fold

--- src/components/templates/HomePageMobile.js
+++ src/components/templates/HomePageMobile.js
@@ -1,11 +1,12 @@
 import React from "react";
 import '../../index.css';
+import HomeAboveFoldMobileMobile from "../molecules/HomeAboveFoldMobile";
 
 
 function HomePageMobile(props) {
   return (
     <>
-
+        <HomeAboveFoldMobileMobile/>
     </>
   );
 }
