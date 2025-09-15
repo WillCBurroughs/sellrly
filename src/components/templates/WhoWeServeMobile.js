adb5ba4d89a2849c890d8eb1b53bfc33c104c719
William <willcburroughs@gmail.com>
2025-09-15 18:53:25 -0400

    Adding completed first version who we serve

--- src/components/templates/WhoWeServeMobile.js
+++ src/components/templates/WhoWeServeMobile.js
@@ -2,12 +2,14 @@ import React from "react";
 import '../../index.css';
 import WhoWeServeAboveFold from "../molecules/WhoWeServeAboveFold";
 import WhoWeServeContainerMobile from "../molecules/WhoWeServeContainerMobile";
+import MobileBottomSection from "../molecules/MobileBottomSection";
 
 function WhoWeServeMobile(props) {
   return (
     <>
     <WhoWeServeAboveFold/>
     <WhoWeServeContainerMobile/>
+    <MobileBottomSection spacer = "WhoWeServeSpacing" prefooterid = "holdingCorrect" prefooterdesktop = "adjustMobile" newclassbigdiv = "newclassbigdiv" />
     </>
   );
 }
