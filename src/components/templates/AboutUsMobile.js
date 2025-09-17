1c9c586f4f37b6b3ec0e04e3fd1191d20d148c93
William <willcburroughs@gmail.com>
2025-09-17 18:39:58 -0400

    Adding start of our team mobile

--- src/components/templates/AboutUsMobile.js
+++ src/components/templates/AboutUsMobile.js
@@ -1,13 +1,15 @@
 import React from "react";
 import '../../index.css';
 import AboutUsMobileFold from "../molecules/AboutUsMobileFold";
-import AboutUsMobileTopSectionsContainer from '../molecules/AboutUsMobileTopSectionsContainer'
+import AboutUsMobileTopSectionsContainer from '../molecules/AboutUsMobileTopSectionsContainer';
+import HoldOurTeamMobile from "../molecules/HoldOurTeamMobile";
 
 function WhoWeServeMobile(props) {
   return (
     <>
         <AboutUsMobileFold/>
         <AboutUsMobileTopSectionsContainer/>
+        <HoldOurTeamMobile/>
     </>
   );
 }
