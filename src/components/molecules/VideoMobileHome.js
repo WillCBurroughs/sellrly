1e19dcfd2de7f1dfb84da817dd31186d01604a58
William <willcburroughs@gmail.com>
2025-09-22 19:53:41 -0400

    adding final component

--- src/components/molecules/VideoMobileHome.js
+++ src/components/molecules/VideoMobileHome.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import '../../index.css';
 import EmphasisIcon from '../atoms/EmphasisIcon';
+import VideoPoster from '../../images/Video-Image-Preview.png';
 
 function VideoMobileHome(props) {
+  const videoRef = useRef(null);
+
+  // Assign src dynamically after mount
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.src = "/video/SocialSelling.mp4";
+    }
+  }, []);
+
+  const handlePlay = () => {
+    const video = videoRef.current;
+    if (video && video.paused) {
+      video.play();
+    }
+  };
+
   return (
     <div className='VideoMobileHome'>
-        <div className='holdEmphasisMobile'>
-            <EmphasisIcon text = "How it Works" />
-        </div>
-        <div>
-            <h1 className='adjustHeaderSize'>
-                What makes Sellrly the best platform for social sellers?
-            </h1>
-        </div>
-        <div className='videoAreaMobile'>
-            <video
-                className="videoPlayer"
-                width="100%"
-                height="auto"
-                id='videoHome'
-                preload="metadata"
-                controls
-            >
-            <source src="/video/SocialSelling.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-            </video>
-        </div>
+      <div className='holdEmphasisMobile'>
+        <EmphasisIcon text="How it Works" />
+      </div>
+      <div>
+        <h1 className='adjustHeaderSize'>
+          What makes Sellrly the best platform for social sellers?
+        </h1>
+      </div>
+      <div className='videoAreaMobile' onClick={handlePlay}>
+        <video
+          ref={videoRef}
+          className="videoPlayer"
+          width="100%"
+          preload="none"
+          controls
+          poster={VideoPoster}
+        >
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 }
